73fd956e7d6f27207233405cafd777b089a5d32c
Jon Borgonia <jon@gomagames.com>
2016-01-09 18:02:36 -1000

    better analog diagonal movement
    movement speed is consistent in every direction

---
+++ source/PlayState.hx
@@ -0,0 +1,183 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxState;
+import flixel.util.FlxColor;
+import flixel.addons.display.shapes.FlxShapeCross;
+import flixel.util.FlxSpriteUtil;
+import flixel.util.FlxMath;
+using flixel.util.FlxAngle;
+import flixel.util.FlxPoint;
+import flixel.util.FlxVelocity;
+import flixel.input.gamepad.FlxGamepad;
+import flixel.input.gamepad.LogitechButtonID;
+
+/**
+ * A FlxState which can be used for the actual gameplay.
+ */
+class PlayState extends FlxState
+{
+  private static inline var shape_size : Float = 50;
+  private var cross_shape : FlxShapeCross;
+  private var shape_velocity : { x : Float, y : Float, rotation : Float };
+  private var game_pad:FlxGamepad;
+
+  /**
+   * Function that is called up when to state is created to set it up.
+   */
+  override public function create():Void
+  {
+    // Hide the mouse
+    FlxG.mouse.visible = false;
+
+    // initialize shape velocity
+    shape_velocity = { x : 0, y : 0, rotation : 0 };
+
+    // setup and create shape
+    var line_style:LineStyle = {
+      color: FlxColor.CHARTREUSE,
+      thickness: 2,
+      pixelHinting: true
+    };
+    var fill_style:FillStyle = {
+      hasFill: true,
+      color: FlxColor.AZURE,
+      alpha: 1.0
+    };
+    cross_shape = new FlxShapeCross(
+      ( FlxG.width - shape_size ) / 2, ( FlxG.height - shape_size ) / 2,
+      shape_size, shape_size/3,
+      shape_size, shape_size/3,
+      0.5, 0.5,
+      line_style,
+      fill_style
+    );
+
+    // smoothen out with anti-aliasing when rotated (affects performance)
+    cross_shape.antialiasing = true;
+
+    // add the shape to this state
+    add(cross_shape);
+
+    // call the superclass create() method *required*
+    super.create();
+  }
+
+  /**
+   * Function that is called when this state is destroyed - you might want to
+   * consider setting all objects this state uses to null to help garbage collection.
+   */
+  override public function destroy():Void
+  {
+    // set sprites to null to help with gc
+    cross_shape = null;
+
+    super.destroy();
+  }
+
+  /**
+   * Function that is called once every frame.
+   */
+  override public function update():Void
+  {
+
+    handleInput();
+
+    handleAnimation();
+
+    super.update();
+  }
+
+  private inline function handleInput():Void
+  {
+    // Important: can be null if there's no active gamepad yet!
+    if (game_pad == null) {
+      game_pad = FlxG.gamepads.lastActive;
+    }else{
+      updateGamepadMovement();
+    }
+
+  }
+
+  private inline function handleAnimation():Void
+  {
+    /*
+      Get the angle of the analog stick in radians
+      Math.atan2(y2 - y1, x2 - x1) * (180 / Math.PI)
+     */
+    var angle = FlxAngle.getAngle(FlxPoint.weak(0,0), FlxPoint.weak(shape_velocity.x, shape_velocity.y)) - 90;
+
+    /*
+      Get the speed based on the analog pressure or "distance" of the joystick
+      Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+     */
+    var speed = FlxMath.getDistance(FlxPoint.weak(0,0), FlxPoint.weak(shape_velocity.x, shape_velocity.y));
+
+    /*
+      Move the shape
+      http://api.haxeflixel.com/flixel/util/FlxVelocity.html#velocityFromAngle
+     */
+    var velocity = FlxVelocity.velocityFromAngle(angle, speed * Reg.MOVE_SPEED);
+    cross_shape.x += velocity.x;
+    cross_shape.y += velocity.y;
+
+    // update the shape's rotation
+    cross_shape.angle += shape_velocity.rotation;
+  }
+
+  private inline function updateGamepadMovement():Void
+  {
+    // Vertical Movement
+    if( game_pad.getAxis(LogitechButtonID.LEFT_ANALOGUE_Y) != 0 ){
+      shape_velocity.y = game_pad.getYAxis(LogitechButtonID.LEFT_ANALOGUE_Y);
+    }else{
+      // stop moving vertically
+      shape_velocity.y *= .9; // set to = 0; to stop hard
+    }
+
+    // Horizontal Movement
+    if( game_pad.getAxis(LogitechButtonID.LEFT_ANALOGUE_X) != 0 ){
+      shape_velocity.x = game_pad.getXAxis(LogitechButtonID.LEFT_ANALOGUE_X);
+    }else{
+      // stop moving horizontally
+      shape_velocity.x *= .9; // set to = 0; to stop hard
+    }
+
+    /*
+      Rotation Control
+      (this registered as X axis for Logitech)
+     */
+    if ( game_pad.getAxis( LogitechButtonID.RIGHT_ANALOGUE_X ) != 0 ){
+
+      // rotate shape
+      shape_velocity.rotation = Reg.ROTATION_SPEED * game_pad.getXAxis( LogitechButtonID.RIGHT_ANALOGUE_X );
+
+    }else{
+
+      // stop rotating
+      shape_velocity.rotation *= .9; // set to = 0; to stop hard
+
+    }
+
+
+    /*
+      Test button presses
+    */
+    if (game_pad.pressed(LogitechButtonID.ONE)) {
+      trace("The X button of the Logitech controller is pressed.");
+    }
+
+    if (game_pad.pressed(LogitechButtonID.TWO)) {
+      trace("The A button of the Logitech controller is pressed.");
+    }
+
+    if (game_pad.pressed(LogitechButtonID.THREE)) {
+      trace("The B button of the Logitech controller is pressed.");
+    }
+
+    if (game_pad.pressed(LogitechButtonID.FOUR)) {
+      trace("The Y button of the Logitech controller is pressed.");
+    }
+
+  }
+}
---
+++ source/Reg.hx
@@ -0,0 +1,16 @@
+package;
+
+import flixel.util.FlxSave;
+
+/**
+ * Handy, pre-built Registry class that can be used to store
+ * references to objects and other things for quick-access. Feel
+ * free to simply ignore it or change it in any way you like.
+ */
+class Reg
+{
+
+  public static inline var MOVE_SPEED = 5; // pixels per frame
+  public static inline var ROTATION_SPEED = 7.5;
+
+}
